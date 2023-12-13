77be314d2e3bd6caab3ec274584606be547798a4
Maria Angeles <maria.d.juri@gmail.com>
2023-12-13 16:38:03 -0500

    background colors are showing up on cards

--- instaGreet/src/components/MainFeed.jsx
+++ instaGreet/src/components/MainFeed.jsx
@@ -1,45 +1,49 @@
-import Card from './Card'
-import FollowingFeed from './FollowingFeed'
-import UserPage from './UserPage'
-import Login from './Login'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import Card from "./Card";
+import FollowingFeed from "./FollowingFeed";
+import UserPage from "./UserPage";
+import Login from "./Login";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const MainFeed = ({ token }) => {
-    const [cardInfo, setCardInfo] = useState([])
-    // const [token, setToken] = useState(null)
-    const [username, setUsername] = useState('')
+  const [cardInfo, setCardInfo] = useState([]);
+  // const [token, setToken] = useState(null)
+  const [username, setUsername] = useState("");
 
-console.log(`this is main feed ${token}`)   
-  
-    useEffect(() => {
-      axios
-        .get('https://social-cards.fly.dev/api/cards/', {
-          headers: {
-            Authorization: `Token ${token}`,
-          },
-        })
-        .then((res) => {
-          setCardInfo(res.data.results)
-        })
-    }, [token])
-    console.log("This is main feed")
-    console.log(typeof(cardInfo))
+  console.log(`this is main feed ${token}`);
 
-    return (
-        <>
-        <h1>Cards</h1>
-      {token &&
-        // <p>{cardInfo}</p>
-        <ul>
-        
+  useEffect(() => {
+    axios
+      .get("https://social-cards.fly.dev/api/cards/", {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      })
+      .then((res) => {
+        setCardInfo(res.data.results);
+      });
+  }, [token]);
+  console.log("This is main feed");
+  console.log(typeof cardInfo);
+
+  return (
+    <>
+      <h1>Cards</h1>
+      {token && (
+        <div>
+          {/* // <p>{cardInfo}</p> */}
           {cardInfo.map((card) => {
-            return <li key={card.id}>{card.front_text}</li>
+            return (
+              <Card
+                key={card.id}
+                front_text={card.front_text}
+                background_color={card.background_color}
+              />
+            );
           })}
-        </ul>
-      
-        }
-            {/* <p>This is MainFeed</p>
+        </div>
+      )}
+      {/* <p>This is MainFeed</p>
             <div className="cardFrame">
                 <div className="card">
                     <Card />
@@ -51,7 +55,8 @@ console.log(`this is main feed ${token}`)
             <UserPage
 
             /> */}
-        </>
-)}
+    </>
+  );
+};
 
-export default MainFeed
+export default MainFeed;
