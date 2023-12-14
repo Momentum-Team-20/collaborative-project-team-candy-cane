b51269e2d30dc20e052763dc79b3b5939f51b058
freddiebell88 <frederikcwbell@gmail.com>
2023-12-14 14:33:49 -0500

    add form input fields for create card component

--- instaGreet/src/components/CreateCard.jsx
+++ instaGreet/src/components/CreateCard.jsx
@@ -1,9 +1,28 @@
 
 const CreateCard = () => {
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+}
+
     return(
         <>
             <p>This is the Create Card Page</p>
-            <button className="newCardButton">Create your new greeting card here!</button>
+            <form method="post" onSubmit={handleSubmit}>
+                <label> Front of Card:<input name="frontText" /></label>
+                <label> Inside of Card:<input name="backText" /></label>
+                <label> Background color:
+                    <select name="selectedBackgroundColor">
+                        <option value="red">Red</option>
+                        <option value="blue">Blue</option>
+                        <option value="green">Green</option>
+                    </select>
+                </label>
+                <button type="submit">Create!</button>
+            </form>
+            {/* I think this button should actually go on the user page? - Freddie
+            <button className="newCardButton">Create your new greeting card here!</button> */}
         </>
     )
 }
