f8642733e344687ae9ec82c8dd8bd28b86628669
freddiebell88 <frederikcwbell@gmail.com>
2023-12-14 15:08:41 -0500

    WIP functions for create card form

--- instaGreet/src/components/CreateCard.jsx
+++ instaGreet/src/components/CreateCard.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 
 const CreateCard = () => {
+    const [form, setForm] = useState({
 
+})
+
+
+const handleChange = (e) => {
+    setForm({
+        ...form,
+        [e.target.id]: e.target.value,
+    })
+}
 const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -10,8 +21,8 @@ const handleSubmit = (e) => {
         <>
             <p>This is the Create Card Page</p>
             <form method="post" onSubmit={handleSubmit}>
-                <label> Front of Card:<input name="frontText" /></label>
-                <label> Inside of Card:<input name="backText" /></label>
+                <label> Front of Card:<input id="front_text" value={form.front_text} onChange={handleChange} /></label>
+                <label> Inside of Card:<input id="back_text" value={form.back_text} onChange={handleChange}/></label>
                 <label> Background color:
                     <select name="selectedBackgroundColor">
                         <option value="red">Red</option>
