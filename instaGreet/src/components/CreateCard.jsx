f6b905f63c52c6d0459d5d73d678451847caf243
freddiebell88 <frederikcwbell@gmail.com>
2023-12-17 20:10:24 -0500

    font picker WIP

--- instaGreet/src/components/CreateCard.jsx
+++ instaGreet/src/components/CreateCard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import axios from 'axios'
 import Card from './Card'
 import { useNavigate } from 'react-router-dom'
+import FontPicker from "font-picker-react";
 
 
 const CreateCard = ({token}) => {
@@ -11,6 +12,7 @@ const CreateCard = ({token}) => {
         background_color:'',
 })
 const navigate = useNavigate()
+const [font, setFont] = useState({})
 
 // add .lowercase
 const handleChange = (e) => {
@@ -50,22 +52,28 @@ const handleSubmit = (e) => {
                 </label>
                 <label>Background Image:<input id="imageURL" value={form.imageURL} onChange={handleChange}/>
                 </label>
-                <label>Font:
+                {/* <label>Font:
                     <select id="font" onChange={handleChange}>
                         <option></option>
                         <option></option>
                         <option></option>
                     </select>
-                </label>
+                </label> */}
+                <FontPicker 
+                    apiKey="AIzaSyCJybl5EEjBi_gC1kHz_QKrQsNPk3B7oLQ"
+                    // activeFontFamily={font}
+                    // onChange={(nextFont) =>
+                    // setFont(nextFont.family)}
+                />
                 <button type="submit">Post!</button>
             </form>
             <Card 
             front_text={form.front_text}
             back_text={form.back_text}
-            background_color={form.background_color}/>
-            
+            background_color={form.background_color}
+            />
         </>
     )
 }
-
+// google fonts API key AIzaSyCJybl5EEjBi_gC1kHz_QKrQsNPk3B7oLQ
 export default CreateCard
