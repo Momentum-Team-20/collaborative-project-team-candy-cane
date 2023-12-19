17a33179bb5e2c7d57d4d108cb0d407d1a6aca80
Maria Angeles <maria.d.juri@gmail.com>
2023-12-18 20:55:25 -0500

    followers can now be listed on a user page

--- instaGreet/src/components/UserPage.jsx
+++ instaGreet/src/components/UserPage.jsx
@@ -7,28 +7,69 @@ import NavBar from "./NavBar";
 
 const UserPage = (props) => {
   const [userCards, setUserCards] = useState([]);
+  const [showFollowers, setShowFollowers] = useState([]);
+
   useEffect(() => {
+// do a thing
+
     axios
-      .get("https://social-cards.fly.dev/api/cards/me/", {
-        headers: {
-          Authorization: `Token ${props.token}`,
-        },
-      })
+      .get(
+        "https://social-cards.fly.dev/api/cards/me/",
+
+        {
+          headers: {
+            Authorization: `Token ${props.token}`,
+          },
+        }
+      )
       .then((results) => {
         console.log("this is results", results);
         setUserCards(results.data);
       });
+
+
+      axios
+      .get(
+        "https://social-cards.fly.dev/api/users/followed",
+
+        {
+          headers: {
+            Authorization: `Token ${props.token}`,
+          },
+        }
+      )
+      .then((results) => {
+        console.log("this is followers results", results);
+        setShowFollowers(results.data.results)
+      });
   }, [props.token]);
+
+  const [expanded, setExpanded] = useState(false);
+  const handleFollowerListExpandedClick = () => {
+    setExpanded(!expanded);
+  };
+
 
   return (
     <>
       <NavBar />
       <div className="userPageContainer">
-        <h1 className="userPageTitle">{props.username}&#39;s InstaGreet Page</h1>
+        <h1 className="userPageTitle">
+          {props.username}&#39;s InstaGreet Page
+        </h1>
         {/* link to followers list below */}
-        <h2 className="userPageFollowingText">
-          Who is {props.username} following?{" "}
-        </h2>
+        <div className="userPageFollowingText">
+          <h2>Who is {props.username} following?{" "}</h2>
+        <button onClick={handleFollowerListExpandedClick} className="followingButton" >
+          {expanded ? "Hide Following" : "Show Following"}
+        </button>
+        {expanded && (
+          <div className="userPageFollowingText">{showFollowers.map((follower) => (
+            <div key={follower.id}>{follower.username}</div>
+            ))}</div>
+            )}
+            </div>
+            <br></br>
         <div className="userPageCardContainer">
           {/* the button below needs to be routed to CreateCard */}
           <div className="userCards">
@@ -37,6 +78,7 @@ const UserPage = (props) => {
                 <a href="/create-card">Create A New Card</a>
               </div>
             </div>
+            {/* hide the button */}
             {userCards.map((card) => {
               return (
                 <Card
