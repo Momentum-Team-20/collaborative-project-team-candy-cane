640c617e0d1cd5ace57ddcb53643fe8f56db505e
freddiebell88 <frederikcwbell@gmail.com>
2023-12-20 14:25:20 -0500

    more styling on create card form

--- instaGreet/src/components/CreateCard.jsx
+++ instaGreet/src/components/CreateCard.jsx
@@ -5,6 +5,8 @@ import NavBar from "./NavBar";
 import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 
 const CreateCard = ({token}) => {
@@ -52,50 +54,65 @@ const handleSubmit = (e) => {
         text_align={form.text_align}
         />
         <Form>
-        <Form.Group method="post" onSubmit={handleSubmit}>
-            <Form.Label> Greeting:<Form.Control id="front_text" value={form.front_text} onChange={handleChange} /></Form.Label>
-        </Form.Group>
-        <Form.Group>    
-            <Form.Label htmlFor="background_color">Color Picker:</Form.Label>
-            <Form.Control type="color" id="background_color" value={"#ffffff"} onChange={handleChange}/>
-        </Form.Group>
-        <Form.Group>
-            <Form.Label>Font:
-                <Form.Select id="font" onChange={handleChange}>
-                    <option>------</option>
-                    <option>Rubik</option>
-                    <option>Playfair</option>
-                    <option>Quicksand</option>
-                </Form.Select>
-            </Form.Label>
-        </Form.Group>
-        <Form.Group>
-            <Form.Label>Font Size:
-                <Form.Select id="font_size" onChange={handleChange}>
-                    <option>-----</option>
-                    <option>small</option>
-                    <option>medium</option>
-                    <option>large</option>
-                    <option>larger</option>
-                    <option>largest</option>
-                </Form.Select>
-            </Form.Label>
-        </Form.Group>
-        <Form.Group>
-            <Form.Label>Align text:
-                <Form.Select id="text_align" onChange={handleChange}>
-                <option>-----</option>
-                <option>center</option>
-                <option>left</option>
-                <option>right</option>
-                <option>top</option>
-                <option>bottom</option>
-                </Form.Select>
-            </Form.Label>
-        </Form.Group>
-        <Form.Group>
-            <Button type="submit">Post!</Button>
-        </Form.Group>
+            <Row>
+                <Col>
+                <Form.Group method="post">
+                    <Form.Label> Greeting:<Form.Control id="front_text" value={form.front_text} onChange={handleChange} /></Form.Label>
+                </Form.Group>
+                </Col>
+                <Col>
+                <Form.Group>    
+                    <Form.Label htmlFor="background_color">Color Picker:</Form.Label>
+                    <Form.Control type="color" id="background_color" value={"#ffffff"} onChange={handleChange}/>
+                </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                <Form.Group>
+                    <Form.Label>Font:
+                        <Form.Select id="font" onChange={handleChange}>
+                            <option>------</option>
+                            <option>Rubik</option>
+                            <option>Playfair</option>
+                            <option>Quicksand</option>
+                            <option>Whisper</option>
+                        </Form.Select>
+                    </Form.Label>
+                </Form.Group>
+                </Col>
+                <Col>
+                <Form.Group>
+                    <Form.Label>Font Size:
+                        <Form.Select id="font_size" onChange={handleChange}>
+                            <option>-----</option>
+                            <option>small</option>
+                            <option>medium</option>
+                            <option>large</option>
+                            <option>larger</option>
+                            <option>largest</option>
+                        </Form.Select>
+                    </Form.Label>
+                </Form.Group>
+                </Col>
+                <Col>
+                <Form.Group>
+                    <Form.Label>Align text:
+                        <Form.Select id="text_align" onChange={handleChange}>
+                        <option>-----</option>
+                        <option>center</option>
+                        <option>left</option>
+                        <option>right</option>
+                        <option>top</option>
+                        <option>bottom</option>
+                        </Form.Select>
+                    </Form.Label>
+                </Form.Group>
+                </Col>
+            </Row>
+            <Form.Group>
+                <Button type="submit" onClick={handleSubmit}>Post!</Button>
+            </Form.Group>
         </Form>
             
         </>
