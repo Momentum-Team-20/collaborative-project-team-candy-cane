0ad5b9da40ea1443d2a95f7d7aa0275b5ae5e5cb
jmatthews64 <jon.matthews64@gmail.com>
2023-12-20 14:27:58 -0500

    Merged with main

--- instaGreet/src/App.jsx
+++ instaGreet/src/App.jsx
@@ -39,11 +39,12 @@ function App() {
         <Route path="/create-card" element={!token ? <Navigate to="/login" /> : <CreateCard token={token} />} />
         <Route path="/user-page" element={!token ? <Navigate  to="/login" /> : <UserPage token={token} username={username} /> } />
         <Route path="/logout" element={!token ? <Navigate to="/" /> : <Logout token={token} setAuth={setAuth} username={username} />} />
-        <Route path="/card-details/:id" element={!token ? <Navigate to="/login" /> : <CardDetails token={token} setCardID={setCardID} username={username} />} />
+        <Route path="/card-details/:id" element={!token ? <Navigate to="/login" /> : 
+          <CardDetails token={token} setCardID={setCardID} username={username} />} />
         <Route path="/delete-card/:id" element={!token ? <Navigate to="/login" /> : <DeleteCard token={token} />} />
-        <Route path="/following-feed" element={!token ? <Navigate to="/login" /> : <FollowingFeed />} />
-        <Route path="/update-card/:id" element={!token ? <Navigate to="/login" /> : <UpdateCard token={token} setCardID={setCardID} username={username} />} />
-
+        <Route path="/following-feed" element={!token ? <Navigate to="/login" /> : <FollowingFeed token={token} />} />
+        <Route path="/update-card/:id" element={!token ? <Navigate to="/login" /> :
+          <UpdateCard token={token} setCardID={setCardID} username={username} />} />
       </Routes>
     </>
   );
--- instaGreet/src/components/FollowingFeed.jsx
+++ instaGreet/src/components/FollowingFeed.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react"
+import axios from "axios"
 import Card from "./Card"
 import NavBar from "./NavBar"
 
-const FollowingFeed = () => {
-    console.log("This is following feed")
+const FollowingFeed = ({ token }) => {
+const [followingIDs, setFollowingIDs] = useState([])
+const [getCards, setGetCards] = useState([])
+
+axios.get("https://social-cards.fly.dev/api/users/followed",
+  {
+    headers: {
+      Authorization: `Token ${token}`,
+    }
+  }
+).then((results) => {
+  console.log("this is followers results in following feed", results.data.results);
+  setFollowingIDs(results.data.results);
+}, [token]);
+
+axios.get("https://social-cards.fly.dev/api/cards",
+      ).then((results) => {
+        console.log("this is cards results in Following feed", results.data.results);
+        setGetCards(results.data.results);
+      }, [token]);
+
+
+console.log("This is following feed")
     return (
         <>
         <NavBar />
